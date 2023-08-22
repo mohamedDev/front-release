ee66293f0640b753dffbe1bc34fc0c635f0eb534
mohamed bouaziz <mohamed_bouaziz@hotmail.fr>
2023-08-22 15:08:02 +0100

    feat(bump): Update bump.js

--- front/bump.js
+++ front/bump.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const fileSystem = require("fs");
-
 let jsonData = require("./package.json");
 const oldBbuildVersion = jsonData.version;
 const buildVersionSplited = oldBbuildVersion.split("+");
@@ -12,7 +11,6 @@ if (buildVersionSplited[1]) {
 const newBuildVersion = buildVersionSplited[0] + "+" + newbuildNumber.toString();
 jsonData.version = newBuildVersion;
 const newJsonData = JSON.stringify(jsonData, null, 2).concat("\n");
-
 fileSystem.writeFile("./package.json", newJsonData, (err) => {
   if (err) throw err;
   console.log("New Build version now is : " + newBuildVersion);
